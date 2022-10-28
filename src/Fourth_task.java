949ca9271610a815292b8b61c1c83de9c17cac2b
moleculoman <Xz2@ZBS@mhSFpg7>
2022-10-28 23:22:43 +0700

    fix for 4th task

--- src/Fourth_task.java
+++ src/Fourth_task.java
@@ -1,15 +1,17 @@
 public class Fourth_task {
     public static void main(String[] args) {
-        int deliveryDistance = 105;
-        if (deliveryDistance < 20) {
-            System.out.println("Потребуется дней: 1");
-        } else if (deliveryDistance >= 20 && deliveryDistance < 60) {
-            System.out.println("Потребуется дней: 2");
-        } else if (deliveryDistance >= 60 && deliveryDistance < 100) {
-            System.out.println("Потребуется дней: 3");
+        int deliveryDistance = 95;
+
+        int deliveryDays = 1;
+
+        if (deliveryDistance > 20) {
+            deliveryDays++;
         }
 
-
+        if (deliveryDistance > 60) {
+            deliveryDays++;
+        }
+            System.out.println("Потребуется дней: "+ deliveryDays);
+        }
     }
-}
 
