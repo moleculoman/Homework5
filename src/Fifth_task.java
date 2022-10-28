5307abddbc7eb67c0d43192bb97d1c65b297a3f5
moleculoman <Xz2@ZBS@mhSFpg7>
2022-10-28 23:11:23 +0700

    Homework 5 (IF statement p.2) done

---
+++ src/Fifth_task.java
@@ -0,0 +1,29 @@
+public class Fifth_task {
+    public static void main(String[] args) {
+        int monthNumber = 13;
+        switch (monthNumber) {
+            case 1:
+            case 2:
+            case 12:
+                System.out.println("Зима");
+                break;
+            case 3:
+            case 4:
+            case 5:
+                System.out.println("Весна");
+                break;
+            case 6:
+            case 7:
+            case 8:
+                System.out.println("Лето");
+                break;
+            case 9:
+            case 10:
+            case 11:
+                System.out.println("Осень");
+                break;
+            default:
+                System.out.println("Такого месяца/периода не существует");
+        }
+        }
+    }
---
+++ src/Fourth_task.java
@@ -0,0 +1,15 @@
+public class Fourth_task {
+    public static void main(String[] args) {
+        int deliveryDistance = 105;
+        if (deliveryDistance < 20) {
+            System.out.println("Потребуется дней: 1");
+        } else if (deliveryDistance >= 20 && deliveryDistance < 60) {
+            System.out.println("Потребуется дней: 2");
+        } else if (deliveryDistance >= 60 && deliveryDistance < 100) {
+            System.out.println("Потребуется дней: 3");
+        }
+
+
+    }
+}
+
